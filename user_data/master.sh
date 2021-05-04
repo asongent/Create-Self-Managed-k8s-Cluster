ce6dbd6786471b8dd37a9f64bf88071aeef5dc6a
asongent <59109755+asongent@users.noreply.github.com>
2021-05-04 12:13:09 -0400

    First commit

---
+++ user_data/master.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo apt-get update -y
+
+sudo apt-get install -y \
+ apt-transport-https \
+ ca-certificates \
+ curl \
+ gnupg-agent \
+ software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+sudo apt-get update -y 
+sudo service docker start
+sudo swapoff –a
+
+
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable kubeadm
+sudo apt-get update -y
+sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+sudo hostnamectl set-hostname master-node
